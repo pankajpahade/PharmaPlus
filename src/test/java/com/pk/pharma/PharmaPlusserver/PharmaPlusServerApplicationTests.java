f9c4152aa9fedad885494e7c9bf47dd8ed42dafd
ppankaj <ppankaj@OJAS00059.OJAS-IT.COM>
2018-05-22 11:35:22 +0530

    Pharma Plus Server Created

---
+++ src/test/java/com/pk/pharma/PharmaPlusserver/PharmaPlusServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pk.pharma.PharmaPlusserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PharmaPlusServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
